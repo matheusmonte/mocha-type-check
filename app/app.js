71ae4902a3455c7bacd80bffaf6b06623a5db524
Matheus Monte <clark.monte1@gmail.com>
2018-08-14 00:09:00 -0400

    refactore function TypeChecker to improve code style
    Signed-off-by: Matheus Monte <clark.monte1@gmail.com>

--- app/app.js
+++ app/app.js
@@ -1,19 +1,22 @@
-module.exports = function TypeChecker(inputObject, typeSpec){
-    if(inputObject === null){
-        return undefined;
-    }
-    if(typeof inputObject === 'array' ){
-        return checkArray(inputObject, typeSpec);
-    }
-    if(typeof inputObject === 'object' ){
-        return checkObject(inputObject, typeSpec);
-    }
-    return checkTypes(inputObject, typeSpec);
+module.exports = function TypeChecker(inputObject, typeSpec){ 
+  if(inputObject === null){
+    return undefined;
+  }
+  switch(typeof inputObject){
+    case 'array':
+      return checkArray(inputObject, typeSpec);
+
+    case 'object':
+      return checkObject(inputObject, typeSpec);
+    
+    default:
+      return checkTypes(inputObject, typeSpec);
+  }
  };
 
 function checkTypes(inputObject, typeSpec){
-    return  typeof inputObject === typeof typeSpec()  ? inputObject :  undefined 
-}
+  return  typeof inputObject === typeof typeSpec()  ? inputObject :  undefined 
+};
 
 function checkArray(inputObject, typeSpec){
   result = new Array();
@@ -23,7 +26,7 @@ function checkArray(inputObject, typeSpec){
     }
   }
   return result;
-}
+};
 
 function checkObject(inputObject, typeSpec){
   result = new Object();
@@ -33,6 +36,6 @@ function checkObject(inputObject, typeSpec){
     }
   }
   return result;
-}
+};
 
 
