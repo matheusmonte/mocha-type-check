eefcf9786e2694b3008269f8c9588ccdcc0a1799
Matheus Monte <clark.monte1@gmail.com>
2018-08-14 00:02:49 -0400

    Add modulaization to improve code reading, starting refactring to open contribution
    Signed-off-by: Matheus Monte <clark.monte1@gmail.com>

--- app/app.js
+++ app/app.js
@@ -3,28 +3,36 @@ module.exports = function TypeChecker(inputObject, typeSpec){
         return undefined;
     }
     if(typeof inputObject === 'array' ){
-        result = new Array();
-        for(let key in inputObject){
-            if(inputObject.hasOwnProperty(key)){
-                typeof inputObject[key] === typeof typeSpec[key]  ? ( result[key] = inputObject[key] ):  undefined           
-            }
-        }
-        return result;
+        return checkArray(inputObject, typeSpec);
     }
     if(typeof inputObject === 'object' ){
-        result = new Object();
-        for(let key in inputObject){
-            if(inputObject.hasOwnProperty(key)){
-                typeof inputObject[key] === typeof typeSpec[key]  ? ( result[key] = inputObject[key] ):  undefined           
-            }
-        }
-        return result;
+        return checkObject(inputObject, typeSpec);
     }
     return checkTypes(inputObject, typeSpec);
  };
 
- function checkTypes(inputObject, typeSpec){
+function checkTypes(inputObject, typeSpec){
     return  typeof inputObject === typeof typeSpec()  ? inputObject :  undefined 
- }
+}
+
+function checkArray(inputObject, typeSpec){
+  result = new Array();
+  for(let key in inputObject){
+    if(inputObject.hasOwnProperty(key)){
+      typeof inputObject[key] === typeof typeSpec[key]  ? ( result[key] = inputObject[key] ):  undefined           
+    }
+  }
+  return result;
+}
+
+function checkObject(inputObject, typeSpec){
+  result = new Object();
+  for(let key in inputObject){
+    if(inputObject.hasOwnProperty(key)){
+      typeof inputObject[key] === typeof typeSpec[key]  ? ( result[key] = inputObject[key] ):  undefined           
+    }
+  }
+  return result;
+}
 
 
